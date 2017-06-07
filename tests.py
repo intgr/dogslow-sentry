25560e07378e8382033667dc9d998f48cd3f1830
Thomas Smith <smithtg@ncbi.nlm.nih.gov>
2017-06-07 10:56:00 -0400

    Failing test for handling unicode surrogates in stack trace

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 import sys
+import datetime
+from collections import namedtuple
 try:
     import _thread as thread
 except ImportError:
@@ -12,3 +14,22 @@ def test_stack():
     frame = sys._current_frames()[thread.get_ident()]
     stack_rendered = dogslow.stack(frame, with_locals=True)
     assert my_variable in stack_rendered
+
+
+MockSettings = namedtuple('MockSettings',
+                          ['DOGSLOW_STACK_VARS'])
+
+
+def test_surrogates(monkeypatch):
+    frame = sys._current_frames()[thread.get_ident()]
+    # Avoid really setting up django :)
+    monkeypatch.setattr(dogslow,
+                        'settings',
+                        MockSettings(DOGSLOW_STACK_VARS=False))
+    # If the bug is present, this will cause a UnicodeEncodeError:
+    stack_rendered = dogslow.WatchdogMiddleware._compose_output(
+        frame,
+        req_string=u'GET \udcee hahaha',
+        started=datetime.datetime.now(),
+        thread_id=thread.get_ident())
+    assert b'\xed\xb3\xae' in stack_rendered
