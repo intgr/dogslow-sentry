087081f5921a5dc230ad58a090956d22153b9d87
Michał Pasternak <michal.dtz@gmail.com>
2018-01-02 13:03:55 +0100

    Django 2.0 support

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,13 @@ def pytest_configure():
     settings.configure(
         SECRET_KEY='...',
         ROOT_URLCONF='tests.urls',
+
+        # Django < 2.0
         MIDDLEWARE_CLASSES=['dogslow.WatchdogMiddleware',],
+
+        # Django >= 2.0
+        MIDDLEWARE=['dogslow.WatchdogMiddleware',],
+        
         TEMPLATES=[
             {
                 'BACKEND': 'django.template.backends.django.DjangoTemplates',
